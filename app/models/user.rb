52b91af88eb367a9201a988cacb8b5717f6e21e8
Natasha Madondo <tasha.madondo@gmail.com>
2021-11-08 19:38:19 +1000

    fixed and migrated devise gem in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
     has_many :user_subjects
     has_many :subjects, through: :user_subjects
     has_one :course, class_name: 'course', foreign_key: 'teacher_id'
     has_many :orders, class_name: 'order', foreign_key: 'student_id'
     has_many :courses, through: :course_users
+    
 end
