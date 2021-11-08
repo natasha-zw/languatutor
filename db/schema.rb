a2871aa4f4150a0e089e509fd1552a1472d6c62f
Natasha Madondo <tasha.madondo@gmail.com>
2021-11-08 20:44:32 +1000

    updated migrations and roles

--- db/schema.rb
+++ db/schema.rb
@@ -10,28 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_08_093432) do
+ActiveRecord::Schema.define(version: 2021_11_08_094423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "course_orders", force: :cascade do |t|
-    t.bigint "course_id", null: false
-    t.bigint "order_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["course_id"], name: "index_course_orders_on_course_id"
-    t.index ["order_id"], name: "index_course_orders_on_order_id"
-  end
-
-  create_table "course_users", force: :cascade do |t|
-    t.bigint "course_id", null: false
-    t.bigint "student_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["course_id"], name: "index_course_users_on_course_id"
-    t.index ["student_id"], name: "index_course_users_on_student_id"
-  end
 
   create_table "courses", force: :cascade do |t|
     t.string "name"
@@ -45,6 +27,24 @@ ActiveRecord::Schema.define(version: 2021_11_08_093432) do
     t.index ["teacher_id"], name: "index_courses_on_teacher_id"
   end
 
+  create_table "courses_orders", force: :cascade do |t|
+    t.bigint "course_id", null: false
+    t.bigint "order_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["course_id"], name: "index_courses_orders_on_course_id"
+    t.index ["order_id"], name: "index_courses_orders_on_order_id"
+  end
+
+  create_table "courses_users", force: :cascade do |t|
+    t.bigint "course_id", null: false
+    t.bigint "student_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["course_id"], name: "index_courses_users_on_course_id"
+    t.index ["student_id"], name: "index_courses_users_on_student_id"
+  end
+
   create_table "orders", force: :cascade do |t|
     t.integer "quanity"
     t.bigint "student_id"
@@ -53,19 +53,20 @@ ActiveRecord::Schema.define(version: 2021_11_08_093432) do
     t.index ["student_id"], name: "index_orders_on_student_id"
   end
 
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
+    t.string "resource_type"
+    t.bigint "resource_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
+    t.index ["resource_type", "resource_id"], name: "index_roles_on_resource"
+  end
+
   create_table "subjects", force: :cascade do |t|
     t.string "name", limit: 20
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "user_subjects", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "subject_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["subject_id"], name: "index_user_subjects_on_subject_id"
-    t.index ["user_id"], name: "index_user_subjects_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -84,13 +85,30 @@ ActiveRecord::Schema.define(version: 2021_11_08_093432) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "course_orders", "courses"
-  add_foreign_key "course_orders", "orders"
-  add_foreign_key "course_users", "courses"
-  add_foreign_key "course_users", "users", column: "student_id"
+  create_table "users_roles", id: false, force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "role_id"
+    t.index ["role_id"], name: "index_users_roles_on_role_id"
+    t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
+    t.index ["user_id"], name: "index_users_roles_on_user_id"
+  end
+
+  create_table "users_subjects", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "subject_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["subject_id"], name: "index_users_subjects_on_subject_id"
+    t.index ["user_id"], name: "index_users_subjects_on_user_id"
+  end
+
   add_foreign_key "courses", "subjects"
   add_foreign_key "courses", "users", column: "teacher_id"
+  add_foreign_key "courses_orders", "courses"
+  add_foreign_key "courses_orders", "orders"
+  add_foreign_key "courses_users", "courses"
+  add_foreign_key "courses_users", "users", column: "student_id"
   add_foreign_key "orders", "users", column: "student_id"
-  add_foreign_key "user_subjects", "subjects"
-  add_foreign_key "user_subjects", "users"
+  add_foreign_key "users_subjects", "subjects"
+  add_foreign_key "users_subjects", "users"
 end
