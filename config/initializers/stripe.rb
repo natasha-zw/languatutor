e2679b3bf444ca53840784cdad29b9f2f41c206c
Natasha Madondo <tasha.madondo@gmail.com>
2021-11-13 21:14:22 +1000

    congifured stripe api keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,1 +1,6 @@
-Stripe.api_key = Rails.application.credentials[:stripe][:secret]
+Rails.configuration.stripe = {
+  public: Rails.application.credentials.stripe[:public],
+  secret_key: Rails.application.credentials.stripe[:secret]
+}
+
+Stripe.api_key = Rails.application.credentials.stripe[:secret]
